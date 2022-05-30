c7d0bc184161707ff7f4038dd3d47d77dac87d15
n-l-i <nyskolmejl@gmail.com>
2022-05-30 18:33:11 +0200

    Remove unused tab

--- Frontend/statistics/statistics.js
+++
@@ -1,9 +0,0 @@
-function get_statistics_data(){
-    make_http_request("GET", 'http://localhost:5000/get_users', {}, load_statistics_data);
-}
-
-function load_statistics_data(response){
-    response.data.forEach((item,index) => {
-        document.getElementById("statistics").innerHTML += item[0]+" - "+item[1]+"<br>";
-    });
-}
