35feac9f6f141ba60a4178e5deb5e0d6d9cc2715
n-l-i <57808975+n-l-i@users.noreply.github.com>
2023-02-02 15:30:51 +0100

    Make sure server process gets killed

--- run.sh
+++ run.sh
@@ -3,13 +3,13 @@
 /bin/bash Deployment/init_website.sh $1 $2 $3 || exit 1
 /bin/bash Deployment/host_website.sh $1 &
 background_pid=$!
-trap "kill $background_pid; exit 1" SIGEXIT SIGHUP SIGINT SIGTERM
+trap "kill $background_pid; pkill flask; exit 1" SIGEXIT SIGHUP SIGINT SIGTERM
 
 while true; do
     sleep 300
     changes=$(git fetch && git rev-list HEAD...origin/main --count)
     if [ $changes -gt 0 ]; then
-        kill $background_pid
+        kill $background_pid; pkill flask
         git pull
         /bin/bash Deployment/host_website.sh $1 &
         background_pid=$!
